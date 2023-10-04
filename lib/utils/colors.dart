441df4b77cb2c11abaae1646a44ad23f823ee000
Ruthika <ruthirockstar2218@gmail.com>
2023-10-04 11:27:35 +0530

    added fonts and colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color textColor = Color(0xFFffcdb2);
+  static const Color mainColor = Color(0xFF6d6875);
+  static const Color iconColor1 = Color(0xFFe5989b);
+  static const Color iconColor2 = Color(0xFFffb4a2);
+  static const Color paraColor = Color(0xFF8f837f);
+  static const Color buttonBackgroundColor = Color(0xFFe5989b);
+  static const Color signColor = Color(0xFFa9a29f);
+  static const Color titleColor = Color(0xFFffcdb2);
+  static const Color roseColor = Color(0xFFb5838d);
+  static const Color mainBlackColor = Color(0xFF332d2b);
+}
